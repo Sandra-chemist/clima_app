c953a94264ec87b7979f9cb2746a59b14463bd54
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-23 13:39:02 +0100

    try/catch block was used

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -15,10 +15,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocation() async {
     LocationPermission permission;
-    permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+
+    try {
+      permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
