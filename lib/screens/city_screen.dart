7016a0c418880931e55fe5aa9724667e64b3e8d7
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-24 11:27:46 +0100

    Navigator.pop was addded

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -7,6 +7,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  late String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +41,14 @@ class _CityScreenState extends State<CityScreen> {
                   ),
                   decoration: kTextFiledInputDecoration,
                   onChanged: (value) {
-                    print(value);
+                    cityName = value;
                   },
                 ),
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: const Text(
                   'Get Weather',
                   style: kButtonTextStyle,
