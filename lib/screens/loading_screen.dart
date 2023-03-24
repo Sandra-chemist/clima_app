7882b60fdc16f0cb44104acb31899ad996ddfc46
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-24 09:02:32 +0100

    Navigator.push was added to LoadingScreen class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
+import 'package:clima_app/screens/location_screen.dart';
 import 'package:clima_app/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima_app/services/location.dart';
 import 'package:clima_app/services/networking.dart';
+import 'location_screen.dart';
 
 const apiKey = '3ae4059ead35a9827fe77d59cd36f4fc';
 
@@ -31,6 +33,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
