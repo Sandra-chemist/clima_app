765969a61ced2c895b7098bd4f2b4b69d55068fc
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-24 10:48:30 +0100

    the Location Methods was refactored

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,7 @@
 import 'package:clima_app/screens/location_screen.dart';
-import 'package:clima_app/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:clima_app/services/location.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = '3ae4059ead35a9827fe77d59cd36f4fc';
+import 'package:clima_app/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,13 +16,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
