b39ab9d8dcc8bfb07594d92660d44c458e71c494
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-24 12:25:52 +0100

    fonts was changed

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 const kTempTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 90.0,
+  fontFamily: 'Amatic SC Bold',
+  fontSize: 100.0,
 );
 
 const kMessageTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 50.0,
+  fontFamily: 'Amatic SC Bold',
+  fontSize: 59.0,
 );
 
 const kButtonTextStyle = TextStyle(
   fontSize: 30.0,
-  fontFamily: 'Spartan MB',
+  fontFamily: 'Amatic SC',
 );
 
 const kConditionTextStyle = TextStyle(
